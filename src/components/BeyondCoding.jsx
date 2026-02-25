23b094c5c24724eb7f104e88053bf48453e63dc2
Klint Ruales <klintruales11@gmail.com>
2026-02-25 22:57:54 +0800

    edit hobbbies

--- src/components/BeyondCoding.jsx
+++ src/components/BeyondCoding.jsx
@@ -10,7 +10,7 @@ const BeyondCoding = () => {
             </div>
             <div className="space-y-4 text-sm leading-tight">
                 <p>I stay active through running, biking, and hiking, which keeps me energized and focused, fueling creativity and productivity in my work.</p>
-                <p>I'm also involved in church activities as multimedia staff and part of the music team, strengthening my teamwork and leadership skills.</p>
+                <p>I'm also involved in church activities, volunteering, and community service, strengthening my teamwork and leadership skills.</p>
             </div>
         </div>
     );
