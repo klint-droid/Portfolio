31ca0072842f30ca804c59b5cc9d14605b0b0f30
Klint Ruales <klintruales11@gmail.com>
2026-02-25 22:52:15 +0800

    add routes to certifications and projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Layout from './sections/Layout.jsx';
+import CertificationLists from "./pages/CertificationLists.jsx";
+import ProjectLists from "./pages/ProjectLists.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return(
-    <div>
-      <Layout />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/all-certifications" element={<CertificationLists />} />
+        <Route path="/all-projects" element={<ProjectLists />} />
+      </Routes>
+    </Router>
   );
 };
 
