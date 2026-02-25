227670dd0269f6d9360112c84c28e34720b21dbc
Klint Ruales <klintruales11@gmail.com>
2026-02-25 23:40:46 +0800

    add tech stacks

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { BiLayer } from 'react-icons/bi'; 
+import { FaJava, FaNodeJs, FaLaravel } from "react-icons/fa";
 import { 
   SiHtml5, SiCss3, SiJavascript, SiReact, SiBootstrap, 
   SiPhp, SiPython, SiMysql, 
-  SiFigma,
-  SiAdobe
+  SiFigma, SiBurpsuite,
+  SiAdobe, SiCanva,
+  SiTailwindcss,
+  SiGit, SiGithub, SiPostman, SiDocker, SiLinux, SiJenkins, SiNpm
 } from 'react-icons/si';
+import { TbBrandCSharp } from "react-icons/tb";
+import { VscVscode } from "react-icons/vsc";
+import { BiLogoPostgresql } from "react-icons/bi";
 
 const TechStack = () => {
     
@@ -26,29 +32,48 @@ const TechStack = () => {
 
             <div className="space-y-6">
                 <div>
-                    {/* 5. Use opacity instead of fixed gray for subheaders so they remain visible in dark mode */}
                     <h3 className="text-xs font-bold uppercase tracking-wider mb-3 opacity-60">Frontend</h3>
                     <div className="flex flex-wrap gap-2">
                         <Badge icon={SiHtml5} name="HTML" colorClass="text-orange-500"/>
                         <Badge icon={SiCss3} name="CSS" colorClass="text-blue-500"/>
                         <Badge icon={SiJavascript} name="JavaScript" colorClass="text-yellow-500"/>
                         <Badge icon={SiReact} name="React" colorClass="text-cyan-500"/>
-                        <Badge icon={SiBootstrap} name="Bootstrap" colorClass="text-purple-500"/>
+                        <Badge icon={SiTailwindcss} name="Tailwind CSS" colorClass="text-purple-500"/>
                     </div>
                 </div>
                 <div>
                     <h3 className="text-xs font-bold uppercase tracking-wider mb-3 opacity-60">Backend & Database</h3>
                     <div className="flex flex-wrap gap-2">
+                        <Badge icon={FaNodeJs} name="Node.js" colorClass="text-green-500"/>
                         <Badge icon={SiPhp} name="PHP" colorClass="text-purple-700"/>
+                        <Badge icon={FaLaravel} name="Laravel" colorClass="text-red-500"/>
                         <Badge icon={SiPython} name="Python" colorClass="text-yellow-600"/>
+                        <Badge icon={FaJava} name="Java" colorClass="text-orange-500"/>
+                        <Badge icon={TbBrandCSharp} name="C#" colorClass="text-blue-600"/>
                         <Badge icon={SiMysql} name="MySQL" colorClass="text-blue-700"/>
+                        <Badge icon={BiLogoPostgresql} name="PostgreSQL" colorClass="text-blue-500"/>
                     </div>
                 </div>
                 <div>
-                    <h3 className="text-xs font-bold uppercase tracking-wider mb-3 opacity-60">Design & Tools</h3>
+                    <h3 className="text-xs font-bold uppercase tracking-wider mb-3 opacity-60">Tools & DevOps</h3>
+                    <div className="flex flex-wrap gap-2">
+                        <Badge icon={VscVscode} name="VS Code" colorClass="text-blue-500"/>
+                        <Badge icon={SiGit} name="Git" colorClass="text-orange-600"/>
+                        <Badge icon={SiGithub} name="GitHub" colorClass="text-gray-800 dark:text-white"/>
+                        <Badge icon={SiPostman} name="Postman" colorClass="text-orange-500"/>
+                        <Badge icon={SiBurpsuite} name="Burp Suite" colorClass="text-green-500"/>
+                        <Badge icon={SiDocker} name="Docker" colorClass="text-blue-500"/>
+                        <Badge icon={SiLinux} name="Linux" colorClass="text-yellow-500"/>
+                        <Badge icon={SiJenkins} name="Jenkins" colorClass="text-red-500"/>
+                        <Badge icon={SiNpm} name="npm" colorClass="text-red-600"/>
+                    </div>
+                </div>
+                <div>
+                    <h3 className="text-xs font-bold uppercase tracking-wider mb-3 opacity-60">Design Tools</h3>
                     <div className="flex flex-wrap gap-2">
                         <Badge icon={SiFigma} name="Figma" colorClass="text-purple-500"/>
                         <Badge icon={SiAdobe} name="Adobe" colorClass="text-blue-500"/>
+                        <Badge icon={SiCanva} name="Canva" colorClass="text-blue-600"/>
                     </div>
                 </div>
             </div>
