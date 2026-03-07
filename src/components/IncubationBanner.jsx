e1d64f8ec9cea3cb0e2ce5b64628877c4e8955aa
Klint Ruales <klintruales11@gmail.com>
2026-03-07 22:41:45 +0800

    add badge

--- src/components/IncubationBanner.jsx
+++ src/components/IncubationBanner.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const IncubationBanner = () => {
   return (
-    <div className="w-full h-1/2 mb-6 rounded-[24px] overflow-hidden border border-[var(--border-color)] shadow-sm hover:shadow-md transition-shadow">
-      <a href="https://www.datacamp.com/portfolio/thisisklint" target="_blank" rel="noopener noreferrer">
+    <div className="w-full h-auto mb-6 rounded-[10px] overflow-hidden border border-[var(--border-color)] shadow-sm hover:shadow-md transition-shadow">
+      <a href="https://www.datacamp.com/certificate/SQA0012000674391" target="_blank" rel="noopener noreferrer">
         <img 
-          src="/Datacamp.png" 
-          alt="Google Developer Groups Scholarship Certificate"
+          src="/badge/SQL Associate - LinkedIn.png" 
+          alt="DataCamp SQL Associate Badge"
           className="w-full h-full object-cover block"
         />
       </a>
