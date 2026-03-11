7d955d02891300658e95e30a5261331e1254fde7
Klint Ruales <klintruales11@gmail.com>
2026-03-11 21:57:43 +0800

    add certificates

--- src/pages/CertificationLists.jsx
+++ src/pages/CertificationLists.jsx
@@ -9,25 +9,85 @@ const CertificationLists = () => {
         window.scrollTo(0, 0);
     }, [])
     const certs = [
-        {
-        name: "Using Python to Interact with the Operating System",
-        issuer: "Coursera",
-        link: "https://www.coursera.org/account/accomplishments/certificate/9Z5K7Z6K8Z3C"
-      },
       {
         name: "Crash Course on Python",
         issuer: "Coursera",
-        link: "https://www.coursera.org/account/accomplishments/certificate/9Z5K7Z6K8Z3C"
+        link: "https://www.coursera.org/account/accomplishments/records/ZLQ3GCRJVF9R"
       },
       {
-        name: "Foundations of Cybersecurity",
+        name: "Using Python to Interact with the Operating System",
         issuer: "Coursera",
-        link: "https://www.coursera.org/account/accomplishments/certificate/9Z5K7Z6K8Z3C"
+        link: "https://www.coursera.org/account/accomplishments/verify/Y0EHUNB69CP8"
       },
       {
-        name: "Tech Talk: Generative AI",
+        name: "Introduction to Git and GitHub",
+        issuer: "Coursera",
+        link: "https://www.coursera.org/account/accomplishments/verify/O5U5QGJ0ZP79"
+      },
+      {
+        name: "Troubleshooting and Debugging Techniques",
+        issuer: "Coursera",
+        link: "https://www.coursera.org/account/accomplishments/verify/934ZJZPNAK1Y"
+      },
+      {
+        name: "Configuration Management and the Cloud",
+        issuer: "Coursera",
+        link: "https://www.coursera.org/account/accomplishments/verify/PLDKR3UZBLZP"
+      },
+      {
+        name: "Accelerate Your Job Search with AI",
+        issuer: "Coursera",
+        link: "https://www.coursera.org/account/accomplishments/records/QUHNCZ9VW3VM"
+      },
+      {
+        name: "Automating Real-World Tasks with Python",
+        issuer: "Coursera",
+        link: "https://www.coursera.org/account/accomplishments/records/QJZHNLC1PDWX"
+      },
+      {
+        name: "Google IT Automation with Python Professional Certificate",
+        issuer: "Coursera",
+        link: "https://www.credly.com/badges/1f934110-13c0-4d34-8c2c-76e1be132fd1/linked_in_profile"
+      },
+      {
+        name: "Foundations: Data, Data, Everywhere",
+        issuer: "Coursera",
+        link: "https://www.coursera.org/account/accomplishments/records/5EDRKGRWQSBK"
+      },
+      {
+        name: "Ask Questions to Make Data-Driven Decisions",
+        issuer: "Coursera",
+        link: "https://www.coursera.org/account/accomplishments/records/33E199N2LWO2"
+      },
+      {
+        name: "Prepare Data for Exploration",
+        issuer: "Coursera",
+        link: "https://www.coursera.org/account/accomplishments/records/AP87DAVZXV9N"
+      },
+      {
+        name: "Process Data from Dirty to Clean",
+        issuer: "Coursera",
+        link: "https://www.coursera.org/account/accomplishments/records/QLMBCRRY7DAO"
+      },
+      {
+        name: "Analyze Data to Answer Questions",
+        issuer: "Coursera",
+        link: "https://www.coursera.org/account/accomplishments/records/13X64ZLS2J27"
+      },
+      {
+        name: "SQL Associate",
+        issuer: "DataCamp",
+        link: "https://www.datacamp.com/certificate/SQA0012000674391"
+      },
+      {
+        name: "Advent of Cyber 2025 Certification",
+        issuer: "tryHackMe",
+        link: "https://tryhackme-certificates.s3-eu-west-1.amazonaws.com/THM-MUOEHIBUHB.pdf"
+      },
+      {
+        name: "Tech Talk in Navigating the Digital Age: Cloud, Generative AI & Cybersecurity",
         issuer: "Accenture",
-        link: "https://www.coursera.org/account/accomplishments/certificate/9Z5K7Z6K8Z3C"
+        link: "https://drive.google.com/drive/u/0/folders/1Y8v0HIpmUh3KPUWkYwSLaKOBryFDY8bQ"
       }
     ]
 
